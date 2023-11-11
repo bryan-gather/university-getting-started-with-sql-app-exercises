c58ff7b13a648d107470a0286d084717509ad4b4
Bryan Phelps <bphelps@gather.town>
2023-11-11 13:35:40 -0800

    exercise 03

---
+++ exercises/vehicles/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO vehicles (
+    id,
+    vehicle_type,
+    purchase_date,
+    serial_number,
+    make,
+    model,
+    year,
+    color,
+    description
+) VALUES (
+    '03d0a3a4-ae36-4178-819c-0c1b08e59afc',
+    'Scooter',
+    '2022-03-07',
+    'SC9757543886484387',
+    'Spitfire',
+    'Inferno',
+    2021,
+    'Red',
+    'Scratch on the left side'
+);
+
+INSERT INTO vehicles (
+    id,
+    vehicle_type,
+    serial_number,
+    make,
+    model,
+    year,
+    color
+) VALUES (
+    '648aefea-9fbc-11ec-b909-0242ac120002',
+    'Skateboard',
+    'SB6694627626486622',
+    'Street Slider',
+    'Motherboard',
+    2020,
+    'Slate Grey'
+);
+
+
+
+INSERT INTO vehicles (
+    vehicle_type,
+    serial_number,
+    make,
+    model,
+    year,
+    color
+) VALUES (
+    'Hoverboard',
+    'SB6694627626486622',
+    'Street Slider',
+    'Motherboard',
+    2020,
+    'Slate Grey'
+), (
+    'Ninja Van',
+    'SB6694627626486622',
+    'Street Slider',
+    'Motherboard',
+    2020,
+    'Carbon Fiber'
+), (
+    'Mystery Mobile',
+    'MM6694627626486622',
+    'Street Slider',
+    'Motherboard',
+    2020,
+    'Carbon Fiber'
+);
