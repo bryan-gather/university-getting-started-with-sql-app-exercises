c624e5cbab19c316eb03b60890210190f1ae6dec
Bryan Phelps <bphelps@gather.town>
2023-11-11 13:20:19 -0800

    exercise 02

---
+++ exercises/vehicles/create_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE vehicles; 
+CREATE TABLE vehicles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    vehicle_type STRING NOT NULL,
+    purchase_date DATE NOT NULL DEFAULT current_date(),
+    serial_number STRING NOT NULL,
+    make STRING NOT NULL,
+    model STRING NOT NULL,
+    year INT2 NOT NULL,
+    color STRING NOT NULL,
+    description STRING
+);
